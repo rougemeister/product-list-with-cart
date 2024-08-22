c5dbc05feab4fd37414cc3ddd0e776d4151ba6a4
rougemeister <rougemeister1@gmail.com>
2024-08-22 13:39:43 +0000

    Render page structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductComponent } from './components/product/product.component';
+import { ProductComponent } from './components/product/product-card.component';
+import { DataService } from './services/data.service';
+import { CartComponent } from "./components/cart/cart.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ProductComponent],
+  imports: [RouterOutlet, ProductComponent, CartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'product-list-with-cart';
+
+  products: any[] = [];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.getItems().subscribe(data => {
+      this.products = data;
+      console.log(this.products[0])
+    });
+  }
 
 }
