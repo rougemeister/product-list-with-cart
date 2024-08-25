0176afef2f4b07217798f0f7207c2803a6d38bb5
rougemeister <rougemeister1@gmail.com>
2024-08-25 09:30:32 +0000

    Updated app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductComponent } from './components/product/product-card.component';
-import { DataService } from './services/data.service';
 import { CartComponent } from "./components/cart/cart.component";
 import { DessertsComponent } from './components/desserts/desserts.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProductComponent, CartComponent, DessertsComponent],
+  imports: [RouterOutlet, ProductComponent, CartComponent, DessertsComponent, ModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
