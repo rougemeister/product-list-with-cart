3181f38e87c1db2413782abba4c4664f71962d71
rougemeister <rougemeister1@gmail.com>
2024-08-25 09:29:23 +0000

    Modified responsive style and added functionality to button

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,inject} from '@angular/core';
+import { ModalService } from '../../services/modal.service';
+import { CartService } from '../../services/cart.service';
+import { Product } from '../../model/product';
 
 @Component({
   selector: 'app-modal',
@@ -8,5 +11,30 @@ import { Component } from '@angular/core';
   styleUrl: './modal.component.scss'
 })
 export class ModalComponent {
+  isVisible: boolean = false;
 
+  emptyCart: number = 0;
+  cartItems: Product[] = [];
+
+  private cartService = inject(CartService);
+  private modalService = inject(ModalService)
+
+  ngOnInit() {
+    this.modalService.modalVisibility$.subscribe(isVisible => {
+      this.isVisible = isVisible;
+    });
+    this.cartService.cartItems$.subscribe((data) => {
+      this.cartItems = data;
+      this.emptyCart = this.cartItems.length;
+    });
+  }
+
+
+  
+
+ 
+
+  closeModal() {
+    this.modalService.close();
+  }
 }
