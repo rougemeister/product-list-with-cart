d5781365f484a23743d07cead06540d15d777f79
rougemeister <rougemeister1@gmail.com>
2024-08-25 09:26:38 +0000

    Added modal service

---
+++ src/app/services/modal.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalService {
+  private modalVisibility = new Subject<boolean>();
+
+  modalVisibility$ = this.modalVisibility.asObservable();
+
+  open() {
+    this.modalVisibility.next(true);
+  }
+
+  close() {
+    this.modalVisibility.next(false);
+  }
+}
